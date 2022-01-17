4ca0bca0cb7ae8e917507798c11b5134f3994429
Manoj Shekar <manojbshekaris@gmail.com>
2022-01-17 23:43:59 +0000

    positive messages so far.

---
+++ run.sh
@@ -0,0 +1,11 @@
+cd /home/pi/Development/Repos/3.4/openvxi-3.4/src/cache
+cd .. && cd VXIinterfaces/ make
+cd .. && cd swichar/ make
+cd .. && cd value/ make
+cd .. && cd trd/ make
+cd .. && cd log/ make
+cd .. && cd cache/ make
+cd .. && cd inet/ make
+cd .. && cd jsi/ make
+cd .. && cd VXI/ make
+cd .. && cd VXIclient/ make
